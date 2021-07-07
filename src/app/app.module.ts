a3a0acf74587077798fb3497e69cd4bc6e0925d2
erianerik <erianerik@gmail.com>
2021-07-07 17:46:11 -0300

    chore: desing do componenet search-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
