f4f9e92f1d0f22da2645ebce0e3e99dfbf20e683
erianerik <erianerik@gmail.com>
2021-07-13 21:04:50 -0300

    chore: html e css do componente create/update finalizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { TableContentComponent } from './components/table-content/table-content.component';
+import { CreateUpdateComponent } from './components/create-update/create-update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     SearchBarComponent,
-    TableContentComponent
+    TableContentComponent,
+    CreateUpdateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/create-update/create-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'create-update',
+  templateUrl: './create-update.component.html',
+  styleUrls: ['./create-update.component.css']
+})
+export class CreateUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
